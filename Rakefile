38f3958335c28b1fe69dfb6b57d3fbbff9cec999
Michael Tighe <striderIIDX@gmail.com>
2012-08-12 13:19:46 -0500

    Fixing out of control tabstops, updating README

--- Rakefile
+++ Rakefile
@@ -3,12 +3,12 @@ require 'rake'
 require 'echoe'
 
 Echoe.new('snowflake', '0.1.0') do |p|
-	p.description = 'Generate a UUID column on ActiveRecord models'
-	p.url         = 'http://github.com/strider-/snowflake'
-	p.author      = 'Michael Tighe'
-	p.email       = 'striderIIDX@gmail.com'
-	p.ignore_pattern = ["tmp/*", "script/*"]
-	p.development_dependencies = []
+  p.description    = 'Generate a UUID column on ActiveRecord models'
+  p.url            = 'http://github.com/strider-/snowflake'
+  p.author         = 'Michael Tighe'
+  p.email          = 'striderIIDX@gmail.com'
+  p.ignore_pattern = ["tmp/*", "script/*"]
+  p.development_dependencies = []
 end
 
 Dir["#{File.dirname(__FILE__)}/tasks/*.rake"].sort.each { |ext| load ext }
--- lib/snowflake.rb
+++ lib/snowflake.rb
@@ -1,38 +1,38 @@
 require 'securerandom'
 
 module Snowflake
-	def self.included(base)
-		base.extend ClassMethods
-	end
+  def self.included(base)
+    base.extend ClassMethods
+  end
 
-	def ensure_unique(name)
-		begin
-			self[name] = yield
-		end while self.class.exists?(name => self[name])
-	end
+  def ensure_unique(name)
+    begin
+      self[name] = yield
+    end while self.class.exists?(name => self[name])
+  end
 
-	module ClassMethods
-		def snowflake(*args, &block)
-			options = { :disable => false }
-			options.merge!(args.pop) if args.last.kind_of? Hash
+  module ClassMethods
+    def snowflake(*args, &block)
+      options = { :disable => false }
+      options.merge!(args.pop) if args.last.kind_of? Hash
 			
-			unless options[:disable]
-				args.each do |name|
-					before_create do |record|
-						if block
-							record.ensure_unique(name, &block)
-						else
-							record.ensure_unique(name) do 
-								SecureRandom.uuid
-							end
-						end
-					end
-				end
-			end
-		end
-	end
+      unless options[:disable]
+        args.each do |name|
+          before_create do |record|
+            if block
+              record.ensure_unique(name, &block)
+            else
+              record.ensure_unique(name) do 
+                SecureRandom.uuid
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
 class ActiveRecord::Base
-	include Snowflake
+  include Snowflake
 end
